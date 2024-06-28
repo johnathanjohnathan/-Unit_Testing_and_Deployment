0ad9d85d9b498b3b8be0baee56fa947665c46a5d
johnathanjohnathan <johnathanvingttrois@gmail.com>
2024-06-28 19:34:08 +0700

    adding cors

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 3000;
 const router = require("./routes");
 const path = require("path");
+const cors = require('cors') 
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/upload", express.static(path.join(__dirname, "/upload")));
